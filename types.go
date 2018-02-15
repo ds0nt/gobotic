750ffcfcbdd29f5c73275507c65324a0c7bc1685
IngCr3at1on <ingcr3at1on@gmail.com>
2018-02-14 19:10:20 -0600

    Comments and make it build (in my test it just crashes)...

--- types.go
+++ types.go
@@ -6,19 +6,22 @@ import (
 	"github.com/ds0nt/gobotic/transports/types"
 )
 
+// Command is a bot command.
 type Command struct {
 	Name    string
 	Help    string
 	Handler func(msg types.MessageEvent, input string) error
 }
 
+// Interceptor is a function to intercept a message.
 type Interceptor func(msg types.MessageEvent) error
 
+// Transport defines a standard transport layer.
 type Transport interface {
 	Connect(ctx context.Context) error
 	OnMessage(types.MessageHandler)
 	OnError(types.ErrorHandler)
-	SendMessage(string)
-	SendError(error)
+	SendMessage(string, string)
+	SendError(string, error)
 	Close() error
 }
